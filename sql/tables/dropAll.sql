ffb5719bcda1b96d01b78a5a4fa6f3a5c3fe0c10
James Raby <james.raby@gmail.com>
2020-09-16 16:57:14 -0700

    add script to drop all tables

---
+++ sql/tables/dropAll.sql
@@ -0,0 +1,20 @@
+
+
+DROP TABLE basal;
+DROP TABLE bolus;
+DROP TABLE cbg;
+DROP TABLE cgm_settings;
+DROP TABLE clinics;
+DROP TABLE clinics_clinicians;
+DROP TABLE clinics_patients;
+DROP TABLE device_event;
+DROP TABLE device_meta;
+DROP TABLE food;
+DROP TABLE old_clinics_patients;
+DROP TABLE physical_activity;
+DROP TABLE pump_settings;
+DROP TABLE smbg;
+DROP TABLE upload;
+DROP TABLE users;
+DROP TABLE wizard;
+
