df6007800b0a3d7b6558f33b5a1141b46bc49ea8
James Raby <james.raby@gmail.com>
2020-09-30 16:41:50 -0700

    update consumer group to reset

--- kafka-database-worker.go
+++ kafka-database-worker.go
@@ -25,7 +25,7 @@ var (
 
 	Partition = 0
 	HostStr, _ = os.LookupEnv("KAFKA_BROKERS")
-	GroupId = "Tidepool-Mongo-Consumer36"
+	GroupId = "Tidepool-Mongo-Consumer38"
 	//MaxMessages = 33100000
 	MaxMessages = 40000000
 	WriteCount = 50000
